f3dffeaeec3ef1eb3568c4b1a7ce16bd9dd59424
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2024-12-11 21:19:30 +0500

    model serialization

--- car_rest/car_app/api_file/serialization.py
+++ car_rest/car_app/api_file/serialization.py
@@ -7,27 +7,17 @@ def alphanumeric(value):
         raise serializers.ValidationError("Car number should be alphanumeric.")
     return value
 
-class CarSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    car_name = serializers.CharField()
-    car_decstr = serializers.CharField()
-    active = serializers.BooleanField(read_only=True)
-    car_number = serializers.CharField(validators=[alphanumeric])
-    price = serializers.DecimalField(max_digits=9, decimal_places=2)
 
-    def create(self, validated_data):
-        # Create a new CarList instance
-        return CarList.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        # Update the CarList instance with validated data
-        instance.car_name = validated_data.get("car_name", instance.car_name)
-        instance.car_decstr = validated_data.get("car_decstr", instance.car_decstr)
-        instance.active = validated_data.get("active", instance.active)
-        instance.car_number = validated_data.get("car_number", instance.car_number)
-        instance.price = validated_data.get("price", instance.price)
-        instance.save()
-
+# Model Serilizer make the table automatic by using that method 
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarList
+        fields = "__all__" # Want when all columns are available
+        # fields = ["id",'car_name','car_decstr']   # Want When Specific Column
+        # exclude =['car_name']
+        
+        
+          
     # Validate for Specific Columns
     def validate_price(self, value):
         if value <= 20000.00:
@@ -41,3 +31,30 @@ class CarSerializer(serializers.Serializer):
                 "Car name and description cannot be the same."
             )
         return data
+    
+    
+
+
+
+# That Code by Hand 
+
+# id = serializers.IntegerField(read_only=True)
+    # car_name = serializers.CharField()
+    # car_decstr = serializers.CharField()
+    # active = serializers.BooleanField(read_only=True)
+    # car_number = serializers.CharField(validators=[alphanumeric])
+    # price = serializers.DecimalField(max_digits=9, decimal_places=2)
+
+    # def create(self, validated_data):
+    #     # Create a new CarList instance
+    #     return CarList.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #     # Update the CarList instance with validated data
+    #     instance.car_name = validated_data.get("car_name", instance.car_name)
+    #     instance.car_decstr = validated_data.get("car_decstr", instance.car_decstr)
+    #     instance.active = validated_data.get("active", instance.active)
+    #     instance.car_number = validated_data.get("car_number", instance.car_number)
+    #     instance.price = validated_data.get("price", instance.price)
+    #     instance.save()
+
